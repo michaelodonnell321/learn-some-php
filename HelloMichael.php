9bd197519a777c2822c27702768699896694e0a2
Michael O'Donnell <gorillaejones@gmail.com>
2019-11-07 20:08:58 -0600

    some string methods added

--- HelloMichael.php
+++
@@ -1,7 +0,0 @@
-<?php
-  $myName = 'Michael';
-  echo "Hello world, and $myName!";
-  // \n to get a new line
-  // . for concatenation
-  echo "\nHey there" . " $myName";
-  ?>
---
+++ HelloMichaelAndStrings.php
@@ -0,0 +1,16 @@
+<?php
+  $myName = 'Michael';
+  echo "Hello world, and $myName!";
+  // \n to get a new line
+  // . for concatenation
+  echo "\nHey there" . " $myName";
+  $say_hello = "Hello $myName";
+  echo "\n$say_hello";
+  $myLastName = "O'Donnell";
+  $full_name = $myName . " " . $myLastName;
+  echo "\n$full_name";
+
+  // strlen to measure length
+  $strlength = "This is a string, it is true";
+  echo strlen("\n$strlength");
+  ?>
