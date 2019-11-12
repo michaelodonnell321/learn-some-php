3d6fc7e46231e736892de3fdb480277e316dc1ce
Michael O'Donnell <gorillaejones@gmail.com>
2019-11-12 09:25:41 -0600

    adds continue example to while loops

--- WhileLoops.php
+++ WhileLoops.php
@@ -8,4 +8,20 @@ while ($counter < 5) {
     echo "Executing: counter is $counter.\n";
 }
 
+//loops are controllable via break and continue
+//break stops the loops immediately
+//countine returns to the top of hte loop and re-checks the condition
+
+$counterTwo = 0;
+
+while ($counterTwo < 20) {
+    $counterTwo += 1;
+
+    if ($counterTwo % 2 == 0) {
+        echo "Skipping number $counterTwo, it is even.\n";
+        continue;
+    }
+
+    echo "Executing: counter is $counterTwo.\n";
+}
 ?>
